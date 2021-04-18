daf618c3e333eadd0afdde8423a7fdc3c363dd52
CJ Quines <cj@cjquines.com>
2021-04-18 13:57:43 -0400

    update db code

--- public/backend/db.py
+++ public/backend/db.py
@@ -1,9 +1,20 @@
 import sqlite3
 
 REAL_COLS = ["rowid", "name", "program", "url", "guesses", "correct"]
-FAKE_COLS = ["rowid", "name", "guesses", "correct"]
+FAKE_COLS = ["rowid", "name", "source", "guesses", "correct", "requests"]
 
 
+def wrap_cursor(f):
+    def wrapped(*args, **kwargs):
+        conn = sqlite3.connect("classes.db")
+        with conn:
+            c = conn.cursor()
+            return f(c, *args, **kwargs)
+
+    return wrapped
+
+
+@wrap_cursor
 def create_table(cur):
     cur.execute(
         """CREATE TABLE real_classes (
@@ -12,19 +23,22 @@ def create_table(cur):
     )
     cur.execute(
         """CREATE TABLE fake_classes (
-            name text, guesses int, correct int
+            name text, source text, guesses int, correct int, requests int
         )"""
     )
 
 
+@wrap_cursor
 def insert_real_classes(cur, classes):
-    cur.executemany("INSERT INTO real_classes VALUES (?, ?, ?, ?, ?)", classes)
+    cur.executemany("INSERT INTO real_classes VALUES (?, ?, ?, 0, 0)", classes)
 
 
+@wrap_cursor
 def insert_fake_classes(cur, classes):
-    cur.executemany("INSERT INTO fake_classes VALUES (?, ?, ?)", classes)
+    cur.executemany("INSERT INTO fake_classes VALUES (?, ?, 0, 0, 0)", classes)
 
 
+@wrap_cursor
 def get_class_pair(cur):
     cur.execute("SELECT rowid, * FROM real_classes ORDER BY guesses LIMIT 1")
     real_class = {a: b for a, b in zip(REAL_COLS, cur.fetchone())}
@@ -33,20 +47,33 @@ def get_class_pair(cur):
     return real_class, fake_class
 
 
-def update_class_pair(cur, real_update, fake_update):
+@wrap_cursor
+def update_real_guess(cur, rowid, correct):
     cur.execute(
-        "UPDATE real_classes SET guesses = ?, correct = ? WHERE rowid = ?", real_update
+        """UPDATE real_classes
+        SET guesses = guesses + 1, correct = correct + ? WHERE rowid = ?""",
+        (int(correct), rowid),
     )
+
+
+@wrap_cursor
+def update_fake_guess(cur, rowid, correct):
     cur.execute(
-        "UPDATE fake_classes SET guesses = ?, correct = ? WHERE rowid = ?", fake_update
+        """UPDATE fake_classes
+        SET guesses = guesses + 1, correct = correct + ? WHERE rowid = ?""",
+        (int(correct), rowid),
+    )
+
+
+@wrap_cursor
+def update_fake_request(cur, rowid):
+    cur.execute(
+        """UPDATE fake_classes
+        SET requests = requests + 1 WHERE rowid = ?""",
+        (rowid,),
     )
 
 
 if __name__ == "__main__":
-    conn = sqlite3.connect(":memory:")
-    with conn:
-        c = conn.cursor()
-        create_table(c)
-        insert_real_classes(c, [("class name", "splash n", "fake", 0, 0)])
-        insert_fake_classes(c, [("fake class", 0, 0)])
-        print(get_class_pair(c))
+    create_table()
+    
